42e1a2c1cf44d851fbba65d651261e7f2e3ad9f7
k1rch <pv-chernov2000@yandex.ru>
2022-03-28 17:33:45 +0700

    Update config.py

--- project/config.py
+++ project/config.py
@@ -1,3 +1,3 @@
 """ Конфигурационный файл. Здесь находится информация для подключения к БД """
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:53bapisi1@127.0.0.1/course_database'
+SQLALCHEMY_DATABASE_URL = 'postgresql://login:pass@127.0.0.1/course_database'
